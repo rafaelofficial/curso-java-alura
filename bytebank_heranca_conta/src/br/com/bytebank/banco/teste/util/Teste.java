a8496e21f4ab5e6ea4c23a7ae26c496fd9807e44
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2023-03-24 14:31:52 -0300

    comparing string

--- bytebank_heranca_conta/src/br/com/bytebank/banco/teste/util/Teste.java
+++ bytebank_heranca_conta/src/br/com/bytebank/banco/teste/util/Teste.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+import br.com.bytebank.banco.modelo.Cliente;
 import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
 
 public class Teste {
 
@@ -14,12 +16,29 @@ public class Teste {
 		List<Conta> lista = new ArrayList<>();
 		
 		Conta cc1 = new ContaCorrente(11, 77);
+		Cliente clienteCC1 = new Cliente();
+		clienteCC1.setNome("Nico");
+		cc1.setTitular(clienteCC1);
 		cc1.deposita(223.0);
-		Conta cc2 = new ContaCorrente(11, 44);
+		
+		
+		Conta cc2 = new ContaPoupanca(11, 44);
+		Cliente clienteCC2 = new Cliente();
+		clienteCC2.setNome("Rafael");
+		cc2.setTitular(clienteCC2);
 		cc2.deposita(155.0);
+		
 		Conta cc3 = new ContaCorrente(11, 33);
+		Cliente clienteCC3 = new Cliente();
+		clienteCC3.setNome("Ana");
+		cc3.setTitular(clienteCC3);
 		cc3.deposita(450.0);
-		Conta cc4 = new ContaCorrente(11, 22);
+		
+		
+		Conta cc4 = new ContaPoupanca(11, 22);
+		Cliente clienteCC4 = new Cliente();
+		clienteCC4.setNome("Paula");
+		cc4.setTitular(clienteCC4);
 		cc4.deposita(99.9);
 		
 		lista.add(cc1);
@@ -32,16 +51,29 @@ public class Teste {
 		}
 		
 		NumeroDaContaComparator comparator = new NumeroDaContaComparator();
+		TitularDaContaComparator titularComparator = new TitularDaContaComparator();
 		
-		lista.sort(comparator);
+		lista.sort(titularComparator);
 		
 		System.out.println("----------");
 
 		for (Conta conta : lista) {
-			System.out.println(conta);
+			System.out.println(conta + ", " + conta.getTitular().getNome());
 		}
 	}
 
+}
+
+class TitularDaContaComparator implements Comparator<Conta> {
+
+	@Override
+	public int compare(Conta o1, Conta o2) {
+
+		String nomeConta1 = o1.getTitular().getNome();
+		String nomeConta2 = o2.getTitular().getNome();
+		
+		return nomeConta1.compareTo(nomeConta2);
+	}
 }
 
 class NumeroDaContaComparator implements Comparator<Conta> {
